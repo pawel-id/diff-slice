c5252cb15ceb62287df5470c8e2cd185d0f28a06
Pawel Idczak <pawel.idczak@orange.com>
2025-01-25 22:02:54 +0100

    add split test

--- test/diff.js
+++ test/diff.js
@@ -1,6 +1,6 @@
-import { deepEqual } from 'node:assert'
+import assert from 'node:assert'
 import { readFileSync } from 'node:fs'
-import { parse, unparse } from 'diff-slice'
+import { parse, split, unparse } from 'diff-slice'
 import { describe, it } from 'node:test'
 
 describe('diff operations', () => {
@@ -8,10 +8,33 @@ describe('diff operations', () => {
     const content = readFileSync('./test/zoo.diff', 'utf8')
     const changes = parse(content)
     const unparsedContent = unparse(changes)
-    deepEqual(
+    assert.deepEqual(
       unparsedContent,
       content,
       'The unparsed content should match the original content'
     )
+  })
+  it('should split diff content into parts correctly', () => {
+    const content = readFileSync('./test/zoo.diff', 'utf8')
+    const changes = parse(content)
+
+    // find a change related to dog.txt file
+    const findDog = (change) =>
+      change.header.some((line) => line.includes('dog.txt'))
+
+    // a dog.txt has two hunks
+    const dogCompleteChange = changes.find(findDog)
+    assert.equal(dogCompleteChange.hunks.length, 2)
+
+    // filter out only hunks that contain the word 'friendly'
+    const { match } = split(changes, {
+      isMatchHunk: (hunk) => {
+        return hunk.lines.some((line) => line.includes('friendly'))
+      },
+    })
+
+    // only one hunk in dog.txt file matches
+    const dogMatchChange = match.find(findDog)
+    assert.equal(dogMatchChange.hunks.length, 1)
   })
 })
